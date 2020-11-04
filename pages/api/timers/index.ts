50b4a8d4a87b3a51ddf1291ae95cf7541ac29076
Ben Johnson <benjamin.d.johnson@icloud.com>
2020-11-03 16:56:24 -0800

    Try out zod

--- pages/api/timers/index.ts
+++ pages/api/timers/index.ts
@@ -1,35 +1,35 @@
 import { NextApiHandler } from "next";
-import { PrismaClient, Timer } from "@prisma/client";
+import { PrismaClient, TimerCreateInput } from "@prisma/client";
+import * as z from "zod";
 
 const prisma = new PrismaClient();
 
+type CreateTimerDto = Pick<TimerCreateInput, "duration" | "name">;
+const createTimerDto = z.object({
+  duration: z.number().positive(),
+  name: z.string().optional().nullable(),
+});
+
 const timersController: NextApiHandler = async (req, res) => {
   try {
-    if (req.method === "GET") {
-      const allTimers = await prisma.timer.findMany();
-      res.status(200).json({ data: allTimers });
+    switch (req.method) {
+      case "GET": {
+        const allTimers = await prisma.timer.findMany();
+        return res.status(200).json({ data: allTimers });
+      }
+      case "POST": {
+        const body: CreateTimerDto = createTimerDto.parse(req.body);
+        const newTimer = await prisma.timer.create({ data: body });
+        return res.status(201).json({ data: newTimer });
+      }
+      default:
+        return res.status(405).json({ message: "Method not allowed" });
     }
-
-    if (req.method === "POST") {
-      // take the parameters from the req
-      const { duration, name } = req.body as Pick<Timer, "duration" | "name">;
-
-      // TODO: validate POST body is properly formatted
-
-      // create timer in the database
-      const newTimer = await prisma.timer.create({
-        data: {
-          duration,
-          name,
-        },
-      });
-
-      res.status(201).json({ data: newTimer });
-    }
-
-    // TODO: 405 method not allowed if any other method
   } catch (error) {
-    console.error(error);
+    if (error instanceof z.ZodError) {
+      return res.status(400).json(error);
+    }
+
     res.status(500).json({ message: "Internal server error" });
   }
 };
